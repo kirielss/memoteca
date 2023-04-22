6cc140e9c8fd2cac5b76d745e6ac79afb5e9708b
skiriel <gabrieldllopes@gmail.com>
2023-04-22 01:21:07 -0300

    going

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    CreateThoughtComponent
   ],
   imports: [
     BrowserModule,
